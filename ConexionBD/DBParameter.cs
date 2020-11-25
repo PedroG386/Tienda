bf002259047b65f927631a71b321af7c7706ed81
Juan Pedro Gamiño Muñoz <muozperi782@hotmail.com>
2020-11-25 16:56:46 -0600

    ,

---
+++ ConexionBD/DBParameter.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConexionBD
+{
+    public class DBParameter
+    {
+        public string Name { get; set; }
+        public ParameterDirection Direction { get; set; }
+        public object Value { get; set; }
+
+        public DBParameter(string NombreParametro, ParameterDirection TipoParametro, object ValorParametro)
+        {
+            Name = NombreParametro;
+            Direction = TipoParametro;
+            Value = ValorParametro;
+        }
+    }
+}
